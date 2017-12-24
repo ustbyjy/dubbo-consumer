15dae5080889f7b9c464cc12524ae77d42854e65
ustbyjy <ustbyjjy@163.com>
2017-12-24 17:59:06 +0800

    启动时检查

--- src/test/java/com/yjy/consumer/ConsumerTestLoadBalance.java
+++ src/test/java/com/yjy/consumer/ConsumerTestLoadBalance.java
@@ -5,8 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Consumer {
-    private static Logger logger = LoggerFactory.getLogger(Consumer.class);
+public class ConsumerTestLoadBalance {
+    private static Logger logger = LoggerFactory.getLogger(ConsumerTestLoadBalance.class);
 
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:dubbo-consumer.xml"});
@@ -16,7 +16,7 @@ public class Consumer {
         Thread.sleep(3000);
 
         DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
-        String hello = demoService.sayHello("World"); // 执行远程方法
+        String hello = demoService.sayHello("world"); // 执行远程方法
         logger.info(hello); // 显示调用结果
     }
 }
