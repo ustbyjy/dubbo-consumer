10b51b122d1ef594f020a6cb542341d80d64c5f9
ustbyjy <ustbyjjy@163.com>
2017-12-24 20:34:47 +0800

    多版本

---
+++ src/test/java/com/yjy/consumer/ConsumerTestMultiVersion.java
@@ -0,0 +1,21 @@
+package com.yjy.consumer;
+
+import com.yjy.provider.service.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConsumerTestMultiVersion {
+    private static Logger logger = LoggerFactory.getLogger(ConsumerTestMultiVersion.class);
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:dubbo-consumer.xml"});
+        context.start();
+        logger.info("服务开始运行...");
+
+        DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
+        DemoService demoService2 = (DemoService) context.getBean("demoService2"); // 获取远程服务代理
+        logger.info(demoService.sayHello("1"));
+        logger.info(demoService2.sayHello("2"));
+    }
+}
